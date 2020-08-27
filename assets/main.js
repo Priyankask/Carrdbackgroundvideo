a8195832fd7a9b8a691f7e427f30612e45070e03
Priyanka Kamashetty <priyankakamashetty@Priyankas-MacBook-Pro.local>
2020-08-27 15:17:59 -0700

    test19

--- assets/main.js
+++ assets/main.js
@@ -760,7 +760,7 @@
 					else
 						this.$target.style.backgroundImage = 'url(\'' + this.poster + '\')';
 						
-						this.$video.onpause = function() {this.$target.style.backgroundImage = 'url(\'' + this.poster + '\')';}		
+						this.$video.onpause = function() {alert('here');this.$target.style.backgroundImage = 'url(\'' + this.poster + '\')';}		
 			};
 	
 	// Container: container01.
